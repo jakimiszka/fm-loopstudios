8f67baebcfbbcf37fc9ae2bc0dd179ff30f83724
jakimiszka <jarek.jakimicha@gmail.com>
2025-12-22 13:29:54 +0100

    1st

---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+import { Card } from "./Card";
+
+export const Container = () => {
+    return (
+        <div className="container flex f-row h-center v-center">
+            <Card />
+        </div>
+    );
+}
